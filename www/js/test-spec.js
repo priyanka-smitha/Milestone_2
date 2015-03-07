192d751a8b19a487075b465494497d008c1bfa18
ssathya <ssathya@ncsu.edu>
2015-03-07 14:32:08 -0500

    condition

--- www/js/test-spec.js
+++ www/js/test-spec.js
@@ -1,13 +1,13 @@
 describe("Person Name", function() {
-  it("Computes fullName based on firstName and lastName", function() {
-    var target = new ViewMyModel("Ada","Lovelace", "abc@xyz.com");
-    expect(target.fullName()).toBe("Ada Lovelace");
-  });
+  // it("Computes fullName based on firstName and lastName", function() {
+    // var target = new ViewMyModel("Ada","Lovelace", "abc@xyz.com");
+    // expect(target.fullName()).toBe("Ada Lovelace");
+  // });
   
-  it("Computes wrongName based on firstName and lastName", function() {
-    var target = new ViewMyModel("Priyanka","Lovelace");
-    expect(target.fullName()).toBe("Priyanka Lovelace");
-  });
+  // it("Computes wrongName based on firstName and lastName", function() {
+    // var target = new ViewMyModel("Priyanka","Lovelace");
+    // expect(target.fullName()).toBe("Priyanka Lovelace");
+  // });
   
   it("Computes wrongName based on firstName and lastName", function() {
     var target = new ViewMyModel("Ada","Shankaran");
